c05a762c1f33bf0198e156f86a4687992dd3c40c
Luka <vujnovacluka89@gmail.com>
2021-12-18 21:28:51 +0100

    Initial Commit

---
+++ Roza/RozaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RozaApp.swift
+//  Roza
+//
+//  Created by Luka Vujnovac on 18.12.2021..
+//
+
+import SwiftUI
+
+@main
+struct RozaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
