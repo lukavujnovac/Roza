500b59d5b2b2283fd34a3b4d3d8cd83c9caa542c
Luka <vujnovacluka89@gmail.com>
2022-01-08 22:31:31 +0100

    cleaner architecture made

--- Roza/Views/GoalListView.swift
+++ Roza/Views/GoalListView.swift
@@ -14,62 +14,19 @@ struct GoalListView: View {
     
     let colorTheme: ColorThemeProtocol = YellowColorTheme()
     @State private var tapped: Bool = false
-    @State private var showingSheet = false
     @State private var goalName: String = ""
     
-    var body: some View {
-        
+    var body: some View {  
         ZStack{
             Rectangle()
                 .ignoresSafeArea()
                 .foregroundColor(colorTheme.background)
-            
             VStack(spacing: 40){
                 VStack {
-                    
-                    TextField("Add goals here...", text: $goalName)
-                        .font(.headline)
-                        .padding(.leading)
-                        .frame(height: 55)
-                        .background(Color("backgroundColor"))
-                        .cornerRadius(10)
-                        .padding(.horizontal, 30)
-                    
-                    Button { 
-                        guard !goalName.isEmpty else {return}
-                        vm.addGoals(text: goalName)
-                        goalName = ""
-                        hideKeyboard()
-                    } label: { 
-                        RoundedRectangle(cornerRadius: 10)
-                            .frame(width: 200, height: 60)
-                            .foregroundColor(colorTheme.primary)
-                            .shadow(color: colorTheme.secondary, radius: 5, x: 0, y: 6)
-                            .overlay(
-                                HStack{
-                                    Image(systemName: "plus.square")
-                                        .foregroundColor(colorTheme.secondary)
-                                    Text("add")
-                                        .fontWeight(.bold)
-                                        .foregroundColor(colorTheme.secondary)
-                                }
-                            )
-                    }
+                    addGoalTextField
+                    addButton                
                 }
-                
-                List {
-                    ForEach(vm.savedEntities) { goal in
-                        GoalCellView(vm: GoalCellViewModel(goal: goal))
-                            .listRowSeparator(.hidden)
-                            .listRowBackground(Color.clear)
-                            .frame(height: 80)
-                            .onTapGesture {
-                                vm.updateGoal(entity: goal)
-                            }
-                    }
-                    .onDelete(perform: vm.deleteGoals)
-                    .padding(.horizontal, 30)
-                }.listStyle(PlainListStyle())
+                goalList
                 Spacer()
             }
         }
@@ -82,36 +39,54 @@ struct ActivityDetailView_Previews: PreviewProvider {
     }
 }
 
-struct AddGoalSheet: View {
-    
-    @StateObject var vm = ActivityListViewModel()
-    
-    @State private var goalText: String = ""
-    
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .ignoresSafeArea()
-                .foregroundColor(Color("lightYellow"))
-            VStack(spacing: 200){
-                TextField("Add New Goal", text: $goalText)
-                    .lineLimit(2)
-                    .padding()
-                    .background(Color("babyBlue").cornerRadius(10))
-                    .padding(.horizontal, 20)
-                
-                
-                Button { 
-                    vm.dataService.goals.append(Goal(name: goalText))
-                    goalText = ""
-                } label: { 
-                    Text("Save")
-                        .foregroundColor(Color("tertiaryColor"))
-                        .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        .padding()
-                        .background(Color("primaryColor").cornerRadius(10))
-                }
-            }
+extension GoalListView {
+    private var addButton: some View {
+        Button { 
+            guard !goalName.isEmpty else {return}
+            vm.addGoals(text: goalName)
+            goalName = ""
+            hideKeyboard()
+        } label: { 
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 200, height: 60)
+                .foregroundColor(colorTheme.primary)
+                .shadow(color: colorTheme.secondary, radius: 5, x: 0, y: 6)
+                .overlay(
+                    HStack{
+                        Image(systemName: "plus.square")
+                            .foregroundColor(colorTheme.secondary)
+                        Text("add")
+                            .fontWeight(.bold)
+                            .foregroundColor(colorTheme.secondary)
+                    }
+                )
         }
     }
+    
+    private var addGoalTextField: some View {
+        TextField("Add goals here...", text: $goalName)
+            .font(.headline)
+            .padding(.leading)
+            .frame(height: 55)
+            .background(Color("backgroundColor"))
+            .cornerRadius(10)
+            .padding(.horizontal, 30)
+    }
+    
+    private var goalList: some View {
+        List {
+            ForEach(vm.savedEntities) { goal in
+                GoalCellView(vm: GoalCellViewModel(goal: goal))
+                    .listRowSeparator(.hidden)
+                    .listRowBackground(Color.clear)
+                    .frame(height: 80)
+                    .onTapGesture {
+                        vm.updateGoal(entity: goal)
+                    }
+            }
+            .onDelete(perform: vm.deleteGoals)
+            .padding(.horizontal, 30)
+        }.listStyle(PlainListStyle())
+    }
+    
 }
