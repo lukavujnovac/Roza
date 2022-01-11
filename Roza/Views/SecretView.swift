1dfed70d0a7017ffa699aa155959ed43f9c8f234
Luka <vujnovacluka89@gmail.com>
2022-01-11 22:34:08 +0100

    code restructured

--- Roza/Views/SecretView.swift
+++ Roza/Views/SecretView.swift
@@ -19,89 +19,18 @@ struct SecretView: View {
             
             VStack(spacing: 60){
                 VStack(spacing: 10) {
-                    Rectangle()
-                        .fill(vm.isSuccess ? colorTheme.secondary : colorTheme.tertiary)
-                        .frame(maxWidth: vm.isComplete ? .infinity : 0)
-                        .frame(height: 55)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .background(Color.gray)
-                        .cornerRadius(10)
-                        .padding(.horizontal, 20)
-                    
+                    statusBar                    
                     HStack {
-                        Text("CLICK AND HOLD HERE")
-                            .foregroundColor(colorTheme.tertiary)
-                            .padding()
-                            .background(colorTheme.primary)
-                            .cornerRadius(10)
-                            .shadow(color: colorTheme.tertiary, radius: 5, x: 0, y: 6)
-                            .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) { (isPressing) in
-                                // start of press -> min duration
-                                if isPressing {
-                                    withAnimation(.easeInOut(duration: 1.0)) {
-                                        vm.isComplete = true
-                                    }
-                                }else {
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { 
-                                        if !vm.isSuccess {
-                                            withAnimation(.easeInOut) {
-                                                vm.isComplete = false
-                                            }
-                                        }
-                                    }
-                                }
-                            } perform: { 
-                                withAnimation(.default) {
-                                    vm.isSuccess = true
-                                    vm.showingAlert = true
-                                }
-                            }
-                        
-                        Text("RESET")
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.black)
-                            .cornerRadius(10)
-                        
-                            .onTapGesture {
-                                withAnimation(.default) {
-                                    vm.isSuccess = false
-                                    vm.isComplete = false
-                                }
-                            }
+                        holdButton
+                        resetButton
                     }
                 }
                 
                 if vm.isSuccess {
                     VStack {
-                        SecureField("Password", text: $vm.passwordText) 
-                            .font(.headline)
-                            .padding(.leading)
-                            .frame(height: 55)    
-                            .background(colorTheme.secondary)
-                            .cornerRadius(10)
-                        .padding(.horizontal, 30)
+                        passwordField
                         
-                        Button {
-                            hideKeyboard()
-                            if vm.password == vm.passwordText {
-                                withAnimation(.default) {
-                                    vm.passwordCorrect = true   
-                                }
-                            }
-                        } label: { 
-                            RoundedRectangle(cornerRadius: 10)
-                                .frame(width: 200, height: 60)
-                                .foregroundColor(colorTheme.primary)
-                                .shadow(color: colorTheme.secondary, radius: 5, x: 0, y: 6)
-                                .overlay(
-                                    HStack{
-                                        Text("submit")
-                                            .fontWeight(.bold)
-                                            .foregroundColor(colorTheme.secondary)
-                                    }
-                                )
-                        }
+                       submitButton
                     }
                 }
                 
@@ -124,3 +53,95 @@ struct SecretView_Previews: PreviewProvider {
         SecretView()
     }
 }
+
+extension SecretView {
+    private var holdButton: some View {
+        Text("CLICK AND HOLD HERE")
+            .foregroundColor(colorTheme.tertiary)
+            .padding()
+            .background(colorTheme.primary)
+            .cornerRadius(10)
+            .shadow(color: colorTheme.tertiary, radius: 5, x: 0, y: 6)
+            .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) { (isPressing) in
+                // start of press -> min duration
+                if isPressing {
+                    withAnimation(.easeInOut(duration: 1.0)) {
+                        vm.isComplete = true
+                    }
+                }else {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { 
+                        if !vm.isSuccess {
+                            withAnimation(.easeInOut) {
+                                vm.isComplete = false
+                            }
+                        }
+                    }
+                }
+            } perform: { 
+                withAnimation(.default) {
+                    vm.isSuccess = true
+                    vm.showingAlert = true
+                }
+            }
+    }
+    
+    private var resetButton: some View {
+        Text("RESET")
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.black)
+            .cornerRadius(10)
+        
+            .onTapGesture {
+                withAnimation(.default) {
+                    vm.isSuccess = false
+                    vm.isComplete = false
+                }
+            }
+    }
+    
+    private var statusBar: some View {
+        Rectangle()
+            .fill(vm.isSuccess ? colorTheme.secondary : colorTheme.tertiary)
+            .frame(maxWidth: vm.isComplete ? .infinity : 0)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.gray)
+            .cornerRadius(10)
+            .padding(.horizontal, 20)
+    }
+    
+    private var passwordField: some View {
+        SecureField("Password", text: $vm.passwordText) 
+            .font(.headline)
+            .padding(.leading)
+            .frame(height: 55)    
+            .background(colorTheme.secondary)
+            .cornerRadius(10)
+            .padding(.horizontal, 30)
+    }
+    
+    private var submitButton: some View {
+        Button {
+            hideKeyboard()
+            if vm.password == vm.passwordText {
+                withAnimation(.default) {
+                    vm.passwordCorrect = true   
+                }
+            }
+        } label: { 
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 200, height: 60)
+                .foregroundColor(colorTheme.primary)
+                .shadow(color: colorTheme.secondary, radius: 5, x: 0, y: 6)
+                .overlay(
+                    HStack{
+                        Text("submit")
+                            .fontWeight(.bold)
+                            .foregroundColor(colorTheme.secondary)
+                    }
+                )
+        }
+    }
+}
+
